0ee6d08cec154a950996ae5d174fe40a674c27bc
“[Kristoffer <[olekfoss@hotmail.no]>
2019-04-25 14:00:18 +0200

    IS-105-gruppe4

--- IS-105-gruppe4/ICA-01/sum/sum_test.go
+++ IS-105-gruppe4/ICA-01/sum/sum_test.go
@@ -10,12 +10,63 @@ var sum_tests_int8 = []struct {
 }{
 	{1, 2, 3},
 	{4, 5, 9},
-	{120, 1, 119},
+	{120, -1, 119},
 }
 
 func TestSumInt8(t *testing.T) {
 	for _, v := range sum_tests_int8 {
 		if val := SumInt8(v.n1, v.n2); val != v.expected {
+			t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
+		}
+	}
+}
+var sum_tests_int32 = []struct {
+	n1       int32
+	n2       int32
+	expected int32
+}{
+	{3456666, -2456666, 1000000},
+	{600000, 40000, 640000},
+	{4569, 5431, 10000},
+}
+
+func TestSumInt32(t *testing.T) {
+	for _, v := range sum_tests_int32 {
+		if val := SumInt32(v.n1, v.n2); val != v.expected {
+			t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
+		}
+	}
+}
+var sum_tests_int64 = []struct {
+	n1       int64
+	n2       int64
+	expected int64
+}{
+	{3456666, -2456666, 1000000},
+	{600000, 40000, 640000},
+	{4569, 5431, 10000},
+}
+
+func TestSumInt64(t *testing.T) {
+	for _, v := range sum_tests_int64 {
+		if val := SumInt64(v.n1, v.n2); val != v.expected {
+			t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
+		}
+	}
+}
+var sum_tests_uint32 = []struct {
+	n1       uint32
+	n2       uint32
+	expected uint32
+}{
+	{3456666, 3543354, 7000020},
+	{600000, 40000, 640000},
+	{4569, 5431, 10000},
+}
+
+func TestSumUint32(t *testing.T) {
+	for _, v := range sum_tests_uint32 {
+		if val := SumUint32(v.n1, v.n2); val != v.expected {
 			t.Errorf("Sum(%d, %d) returned %d, expected %d", v.n1, v.n2, val, v.expected)
 		}
 	}
